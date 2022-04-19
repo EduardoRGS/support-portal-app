0f85977260ec5f8d2eefb7b5f4c6f1493714037b
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-04-19 19:34:46 -0300

    Adicionando método de registro de um novo usuário e modificando o nome do component user

--- support-portal-app/src/app/service/authentication.service.ts
+++ support-portal-app/src/app/service/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { UserComponent } from '../model/user/user.component';
+import { User } from '../model/user/user.component';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +12,12 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  public login(user: UserComponent) : Observable<HttpResponse<any> | HttpErrorResponse> {
+  public login(user: User) : Observable<HttpResponse<any> | HttpErrorResponse> {
     return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
+  }
+
+  public register(user: User) : Observable<User | HttpErrorResponse> {
+    return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user);
   }
 
 }
