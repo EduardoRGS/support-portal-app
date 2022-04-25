552e27178a7304af46979fa66e86360dfc408a11
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-04-25 19:00:17 -0300

    Modificações na service de autenticação e recriando modelo do usuário

--- support-portal-app/src/app/service/authentication.service.ts
+++ support-portal-app/src/app/service/authentication.service.ts
@@ -1,33 +1,29 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import { User } from '../model/user/user.component';
+import { User } from '../model/user';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({providedIn: 'root'})
 export class AuthenticationService {
-  private host = environment.apiUrl;
-  private token: any;
-  private loggedInUsername: any;
+  public host = environment.apiUrl;
+  private token: string = "";
+  private teste: string = "";
+  private loggedInUsername: string = "";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public login(user: User) : Observable<HttpResponse<any> | HttpErrorResponse> {
-    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
+  public login(user: User): Observable<HttpResponse<User>> {
+    return this.http.post<User>(`${this.host}/user/login`, user, { observe: 'response' });
   }
 
-  public register(user: User) : Observable<User | HttpErrorResponse> {
-    return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user);
+  public register(user: User): Observable<User> {
+    return this.http.post<User>(`${this.host}/user/register`, user);
   }
 
-  public logout() : void {
-    this.token = null;
-    this.loggedInUsername = null;
-    localStorage.removeItem('user');
-    localStorage.removeItem('token');
-    localStorage.removeItem('users');
+  public logOut(): void {
+    localStorage.clear();
   }
 
   public saveToken(token: string): void {
@@ -35,8 +31,20 @@ export class AuthenticationService {
     localStorage.setItem('token', token);
   }
 
-  public addUserLocalCache(user: User): void {
+  public addUserToLocalCache(user: User): void {
     localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUserFromLocalCache(): User {
+    return JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  public loadToken(): void {
+    this.token = localStorage.getItem('token') || "";
+  }
+
+  public getToken(): string {
+    return this.token;
   }
 
 }
