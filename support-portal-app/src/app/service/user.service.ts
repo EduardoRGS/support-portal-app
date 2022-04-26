5d62609c3c739d5a44210129ddb337ae5bc249c8
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-04-26 19:46:39 -0300

    Adicionando funcionalidade de adicionar, editar e listar usuario

--- support-portal-app/src/app/service/user.service.ts
+++ support-portal-app/src/app/service/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  public host = environment.apiUrl;
+  
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/user/all`);
+  }
+
+  public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
+
+   public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.put<User>(`${this.host}/user/update`, formData);
+  }
+  
 }
