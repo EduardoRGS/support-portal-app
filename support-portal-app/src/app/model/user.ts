562878226f7dc237d914f81f1e18a79d2d76d089
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-04-27 20:10:27 -0300

    Adicionando os dados do usuário no model

--- support-portal-app/src/app/model/user.ts
+++ support-portal-app/src/app/model/user.ts
@@ -1,3 +1,36 @@
 export class User {
+    public id: number;
+    public userId: string;
+    public fistName: string;
+    public lastName: string;
+    public username: string;
+    public password: string;
+    public email: string;
+    public profileImageUrl: string;
+    public lastLoginDate: Date;
+    public lastLoginDateDisplay: Date;
+    public joinDate: Date;
+    public role: string;
+    public authorities: [];
+    public active: boolean;
+    public notLocked: boolean;
+
+    constructor(){
+        this.id = 0;
+        this.userId = '';
+        this.fistName = '';
+        this.lastName = '';
+        this.username = '';
+        this.password = '';
+        this.email = '';
+        this.profileImageUrl = '';
+        this.lastLoginDate = new Date;
+        this.lastLoginDateDisplay = new Date;
+        this.joinDate = new Date;
+        this.active = false;
+        this.notLocked = false;
+        this.role = '';
+        this.authorities = [];
+    }
     
 }
