d3890fdfe238eb8e9f0429c6220bfe37c2f82512
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-06-26 16:46:46 -0300

    Aplicando modificações no cadastro e login do usuário

--- support-portal-app/src/app/service/user.service.ts
+++ support-portal-app/src/app/service/user.service.ts
@@ -57,6 +57,7 @@ export class UserService {
     formData.append('fistName', user.firstName);
     formData.append('lastName', user.lastName);
     formData.append('username', user.username);
+    formData.append('password', user.password);
     formData.append('email', user.email);
     formData.append('role', user.role);
     formData.append('profileImage', profileImage);
